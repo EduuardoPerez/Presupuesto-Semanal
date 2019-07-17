e8e93781c5d619d829602e40684174fc63f6fe36
Eduardo Perez <edujpp1@gmail.com>
2019-07-17 07:07:45 -0300

    Se implementó la función agregarPresupuesto, a través del state error permite que se imprima un cartel de alerta al usuario

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,16 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function Pregunta(){
+
+  // Definir el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+
+  // Validar el presupuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    // Validar
+    if(cantidad<1 || isNaN(cantidad)){
+      guardarError(true);
+      return;
+    }
+
+    // Si pasa la validación
+  }
 
     return(
       <Fragment>
         <h2>Coloca tu presupuesto</h2>
-        <form>
+
+        {error ? <p className="alert alert-danger error">El presupuesto es incorrecto</p> : null }
+
+        <form
+          onSubmit={agregarPresupuesto}
+        >
           <input 
             type="number"
             className="u-full-width"
             placeholder="Agrega tu presupuesto"
-            //onChange={}
+            onChange={e => guardarCantidad( parseInt(e.target.value, 10) )}
           />
           <input type="submit" className="button-primary u-full-width" value="Definir presupuesto"/>
         </form>
